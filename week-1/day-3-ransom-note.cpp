e589d7e8a6f802358c4c7949a0df3120dfc9337a
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-05-03 12:40:20 +0530

    added solution to week 1, day 3 ransom note problem in may leetcoding challenge

---
+++ week-1/day-3-ransom-note.cpp
@@ -0,0 +1,34 @@
+// Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+// Each letter in the magazine string can only be used once in your ransom note.
+
+// Note:
+// You may assume that both strings contain only lowercase letters.
+
+// canConstruct("a", "b") -> false
+// canConstruct("aa", "ab") -> false
+// canConstruct("aa", "aab") -> true
+
+class Solution {
+public:
+    bool canConstruct(string a, string b) {
+        unordered_map<char,int> m;
+        for(char c: b) {
+            if(m[c]==0) {
+                m[c]=1;
+            }
+            else {
+                m[c]++;
+            }
+        }
+        for(char c:a) {
+            if(m[c]<=0) {
+                return false;
+            }
+            else {
+                m[c]--;
+            }
+        }
+        return true;
+    }
+};
