6b13222a1487c938f35c9e764cad4df65f74345b
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-05-12 15:44:18 +0530

    added solution for week 2 day 12 single element in a sorted array problem

---
+++ week-2/day-10-find-the-town-judge.cpp
@@ -0,0 +1,67 @@
+// Find the Town Judge
+// Solution
+// In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+
+// If the town judge exists, then:
+
+// The town judge trusts nobody.
+// Everybody (except for the town judge) trusts the town judge.
+// There is exactly one person that satisfies properties 1 and 2.
+// You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+
+// If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+
+ 
+
+// Example 1:
+
+// Input: N = 2, trust = [[1,2]]
+// Output: 2
+// Example 2:
+
+// Input: N = 3, trust = [[1,3],[2,3]]
+// Output: 3
+// Example 3:
+
+// Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+// Output: -1
+// Example 4:
+
+// Input: N = 3, trust = [[1,2],[2,3]]
+// Output: -1
+// Example 5:
+
+// Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+// Output: 3
+ 
+
+// Note:
+
+// 1 <= N <= 1000
+// trust.length <= 10000
+// trust[i] are all different
+// trust[i][0] != trust[i][1]
+// 1 <= trust[i][0], trust[i][1] <= N
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& t) {
+        vector<int> v(n+1,0);
+        for(auto i:t) {
+            v[i[1]]++;
+        }
+        int j=-1;
+        for(int i=1;i<=n;i++)
+        {
+            if(v[i]==n-1) {
+                j=i;
+                break;
+            }
+        }
+        // cout<<j;
+        for(auto i:t) {
+            if(i[0]==j) return -1;
+        }
+        return j;
+    }
+};
---
+++ week-2/day-9-valid-perfect-square.cpp
@@ -0,0 +1,45 @@
+// Valid Perfect Square
+// Solution
+// Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+// Note: Do not use any built-in library function such as sqrt.
+
+// Example 1:
+
+// Input: 16
+// Output: true
+// Example 2:
+
+// Input: 14
+// Output: false
+
+class Solution {
+public:
+    // double squareRoot(int n) 
+    // { 
+    //     return pow(2, 0.5*log2(n)); 
+    // } 
+    
+    bool isPerfectSquare(int num) {
+        if(num<0) return false;
+        if(num==0||num==1) return true;
+        long l=1;
+        long r=num/2;
+        long mid=0;
+        long long midSqr=0;
+        while(l<=r) {
+            mid = l +(r-l)/2;
+            midSqr = mid*mid;
+            if(midSqr==num) {
+                return true;
+            }
+            else if(midSqr < num) {
+                l=mid+1;
+            }
+            else {
+                r=mid-1;
+            }
+        }
+        return false;
+    }
+};
